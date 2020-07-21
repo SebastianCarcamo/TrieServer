611353608516af0967b0e707984559e2a004e1e1
SebastianCarcamo <sebastian.carcamo@utec.edu.pe>
2020-07-21 03:50:09 -0500

    fixed suggestions

--- TrieSingleton/trie.h
+++ TrieSingleton/trie.h
@@ -83,7 +83,9 @@ vector<string> suggestWords(string prefix, trieNode *root) {
 }
 
 vector<string> nFirst(vector<string> vec, int n) {
-
+if (vec.size() == 0){
+	return vec;
+}
   std::partial_sort(vec.begin(), vec.begin() + n,vec.end(), [](const std::string& first, const std::string& second){
         return first.size() < second.size();
     });
